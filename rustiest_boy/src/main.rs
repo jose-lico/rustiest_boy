5333d21ad125f92c7fa307f424e8fea8a76313ed
José Lico <57019940+jose-lico@users.noreply.github.com>
2022-07-13 22:13:06 +0100

    Read .gb binary file

--- rustiest_boy/src/main.rs
+++ rustiest_boy/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
+
 fn main() {
-    println!("Hello, world!");
+    let filename: String = "../roms/Tetris.gb".to_string();
+    let contents = get_file_as_byte_vec(&filename);
+        
+    println!("{:x?}", contents);
+}
+
+
+fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
 }
